0f7c31982d1084ab7884261844f26866a6816a50
zwierdl <fa08nofi@stud.informatik.uni-erlangen.de>
2021-12-21 15:37:59 +0100

    der rest (Thomas hats leider vergeigt)

--- project/factorio/cpp_files/events.cpp
+++ project/factorio/cpp_files/events.cpp
@@ -1,12 +1,61 @@
-class event {
+struct event {
   const uint_t timestamp;
 };
 
-class factory_event : public(event){
-public:
+struct victory_event:  public event {
+  victory_event(const uint_t& timestamp) : event::timestamp(timestamp) {};
+};
+
+struct factory_event : public event {
   factory_event(const uint_t& timestamp, const uint_t& factory_id) : event::timestamp(timestamp), factory_id(factory_id) {};
 
   const uint_t factory_id;
 };
 
-class build_factory_event
+struct build_factory_event : public factory_event {
+  build_factory_event(
+    const uint_t& timestamp,
+    const uint_t& factory_id,
+    const std::string& factory_type,
+    const std::string& factory_name
+  ) :
+  event::timestamp(timestamp),
+  factory_event::factory_id(factory_id),
+  factory_type(factory_type),
+  factory_name(factory_name)
+   {};
+
+  const std::string factory_type;
+  const std::string factory_name;
+};
+
+struct destroy_factory_event : public factory_event {
+  destroy_factory_event(
+    const uint_t& timestamp,
+    const uint_t& factory_id
+  ) :
+  event::timestamp(timestamp),
+  factory_event::factory_id(factory_id) {};
+};
+
+struct stop_factory_event : public factory_event {
+  stop_factory_event(
+    const uint_t& timestamp,
+    const uint_t& factory_id
+  ) :
+  event::timestamp(timestamp),
+  factory_event::factory_id(factory_id) {};
+};
+
+struct start_factory_event : public factory_event {
+  build_factory_event(
+    const uint_t& timestamp,
+    const uint_t& factory_id,
+    const std::string& recipe
+  ) :
+  event::timestamp(timestamp),
+  factory_event::factory_id(factory_id),
+  recipe(recipe) {};
+
+  const std::string recipe;
+};
